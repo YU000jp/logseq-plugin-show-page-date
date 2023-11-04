a931a3bf672a40697113427026a1f252ab4f17b4
YU <111847207+YU000jp@users.noreply.github.com>
2023-11-04 14:24:35 +0900

    feat(): init

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,87 @@
-import '@logseq/libs'; //https://plugins-doc.logseq.com/
-import { settingsTemplate } from './settings';
-import { setup as l10nSetup, t } from "logseq-l10n"; //https://github.com/sethyuan/logseq-l10n
-import ja from "./translations/ja.json";
-
+import '@logseq/libs' //https://plugins-doc.logseq.com/
+import { settingsTemplate } from './settings'
+import { setup as l10nSetup, t } from "logseq-l10n" //https://github.com/sethyuan/logseq-l10n
+import ja from "./translations/ja.json"
+import { loadPageInfoButton } from "./pageInfoButton"
+import { loadPageDateNotifier } from "./pageDateNotifier"
 
 /* main */
-const main = () => {
-  (async () => {
-    try {
-      await l10nSetup({ builtinTranslations: { ja } });
-    } finally {
-      /* user settings */
-      logseq.useSettingsSchema(settingsTemplate());
-      if (!logseq.settings) setTimeout(() => logseq.showSettingsUI(), 300);
+const main = async () => {
+
+  await l10nSetup({ builtinTranslations: { ja } })
+
+  /* user settings */
+  logseq.useSettingsSchema(settingsTemplate())
+  if (!logseq.settings) setTimeout(() => logseq.showSettingsUI(), 300)
+
+
+  logseq.provideStyle(String.raw`
+  body>div#root>div>main {
+    & article>div[data-id="show-page-date"] {
+        & div.heading-item {
+          margin-top: 3em;
+          border-top-width: 1px;
+          border-bottom-width: 0;
+          padding-top: 1em;
+          &>h2 {
+            margin-bottom: 0.5em;
+          }
+        }
+        & textarea.form-input {
+          height: 12em;
+          font-size: unset;
+        }
+        & div.desc-item {
+          & p {
+              margin-top: 0.5em;
+              margin-bottom: 0.5em;
+          }
+        }
     }
-  })();
+    & div#main-content-container div {
+        &#injected-ui-item-pageInfoBarSpace-show-page-date.injected-ui-item-pagebar {
+          order:-1;
+        }
+        &#pageBar--pageInfoBarSpace {
+          height: 80px;
+          user-select: none;
+          overflow: auto;
+          & table {
+            width: fit-content;
+            overflow: auto;
+            &>tbody>tr {
+              &>th,
+              &>td {
+                font-size:.8em;
+                padding: unset;
+                padding-left: .8em;
+                width: fit-content;
+              }
+            }
+          }
+        }
+        &.list-wrap:has(div#pageBar--pageInfoBarSpace) {
+          padding-top:unset;
+          overflow:hidden;
+          height:100px;
+        }
+        &[data-type="pagebar"] div.list-wrap:has(div#pageBar--pageInfoBarSpace) {
+        overflow:hidden;
+        }
+      }
+    }
+  }
+`)
+
+  //Page bar item
+  //ページ情報を表示する
+  if (logseq.settings!.loadPageInfoButton === true) loadPageInfoButton()
+
+  //Page date notifier
+  if (logseq.settings!.loadPageDateNotifier === true) loadPageDateNotifier()
+
+}/* end_main */
 
 
-};/* end_main */
 
-
-
-logseq.ready(main).catch(console.error);
+logseq.ready(main).catch(console.error)
--- src/settings.ts
+++ src/settings.ts
@@ -1,13 +1,50 @@
-import { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user';
+import { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user'
+import { t } from 'logseq-l10n'
 
 /* user setting */
 // https://logseq.github.io/plugins/types/SettingSchemaDesc.html
 export const settingsTemplate = (): SettingSchemaDesc[] => [
-    {
-        key: "heading001",
-        type: "heading",
-        title: "no settings",
-        default: "",
-        description: "",
-    },
-];
+  {
+    //loadPageInfo
+    key: "headingLoadPageInfoButton",
+    title: t("Page Info Button feature"),
+    type: "heading",
+    default: "",
+    //ページ名の横にある📋アイコンをクリックしたときに表示されるページ情報を表示する
+    //日付はre-indexを実行すると更新される
+    description: t("Show the page info when click 📋 icon on the right of page title."),
+  },
+  {
+    //loadPageInfo
+    key: "loadPageInfoButton",
+    title: t("Enable"),
+    type: "boolean",
+    default: false,
+    description: "",
+  },
+
+  {
+    //loadPageDateNotifier
+    key: "headingLoadPageDateNotifier",
+    title: t("Page Date Notifier feature"),
+    type: "heading",
+    default: "",
+    description: t("Show the date when a page opens"),
+  },
+  {
+    //loadPageDateNotifier
+    key: "loadPageDateNotifier",
+    title: t("Enable"),
+    type: "boolean",
+    default: true,
+    description: "",
+  },
+  {
+    //created-atを表示しない
+    key: "pageDateNotifierCreatedAt",
+    title: `${t("Enable")}: ${t("Created-at")}`,
+    type: "boolean",
+    default: true,
+    description: t("⚠️Performing a `re-index` in Logseq updates the dates of `created-at` for all pages, causing them to be inadvertently changed."),
+  },
+]
